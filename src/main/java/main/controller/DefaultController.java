d323c33977b0fc27e69217b51b2abe6b8244db29
Dontsov <vova-moove@yandex.ru>
2020-04-13 17:01:00 +0300

    First stage

---
+++ src/main/java/main/controller/DefaultController.java
@@ -0,0 +1,17 @@
+package main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DefaultController
+{
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model)
+    {
+        return "redirect:/templates/index.html";
+    }
+}
